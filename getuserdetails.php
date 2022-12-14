16c0a7a1282af8af074528b6a256f4429ae33720
Akshat53 <singh.akshat.akshat26@gmai.com>
2022-12-15 01:46:01 +0530

    a

--- getuserdetails.php
+++ getuserdetails.php
@@ -4,7 +4,6 @@ header("Access-Control-Allow-Headers: access");
 header("Access-Control-Allow-Methods: GET");
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
-
 require __DIR__ . '/classes/db.config.php';
 require __DIR__ . '/middlewares/auth.middleware.php';
 require __DIR__ . '/classes/error.handler.php';
